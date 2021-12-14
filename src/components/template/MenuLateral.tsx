fabb10802166e7d48dd128bbb5a05e3341eb314d
vitor <vhrocha17@gmail.com>
2021-12-14 09:02:13 -0300

    inicio de projeto do admin template

---
+++ src/components/template/MenuLateral.tsx
@@ -0,0 +1,10 @@
+
+
+export default function MenuLateral(props) {
+  return (
+    <aside>
+      <h1>Menu</h1>
+    </aside>
+
+  )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import Layout from '../components/template/Layout';
+
+export default function Home() {
+  return (
+    <Layout titulo="Pagina Inicial" subtitulo="Estamos contruindo um Layout de admin">
+      <h3>Conteudo</h3>
+    </Layout>
+  )
+}
